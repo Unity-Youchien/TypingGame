41a9eb4a502b5420695ab3dbb7958615c9d14d51
Jonny <53932961+Project-Jonny@users.noreply.github.com>
2022-02-13 23:23:32 +0900

    ランダムに問題を出す

--- Assets/Scripts/TypingManager.cs
+++ Assets/Scripts/TypingManager.cs
@@ -12,17 +12,32 @@ public class TypingManager : MonoBehaviour
     [SerializeField] Text aText; // 答え用のテキスト
 
     // 問題を用意しておく
-    private string _furigana = "たいぴんぐ";
-    private string _question = "タイピング";
-    private string _answer = "taipingu";
+    private string[] _furigana = { "たいぴんぐ", "もんだい", "ようちえん" };
+    private string[] _question = { "タイピング", "問題", "幼稚園" };
+    private string[] _answer = { "taipingu", "mondai", "youchien" };
+
+    // 何番目か指定するためのstring
+    private string _fString;
+    private string _qString;
+    private string _aString;
+
+    // 何番目の問題か
+    private int _qNum;
 
     // ゲームを始めた時に1度だけ呼ばれるもの
     void Start()
     {
+        // _qNumに０〜問題数の数までのランダムな数字を1つ入れる
+        _qNum = Random.Range(0, _question.Length);
+
+        _fString = _furigana[_qNum];
+        _qString = _question[_qNum];
+        _aString = _answer[_qNum];
+
         // 文字を変更する
-        fText.text = _furigana;
-        qText.text = _question;
-        aText.text = _answer;
+        fText.text = _fString;
+        qText.text = _qString;
+        aText.text = _aString;
     }
 
     // Update is called once per frame
