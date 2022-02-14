aa1aabcc9930e0e525bff9d015e14e005f7e0e71
Jonny <53932961+Project-Jonny@users.noreply.github.com>
2022-02-14 23:38:37 +0900

    正解処理と問題の変更

--- Assets/Scripts/TypingManager.cs
+++ Assets/Scripts/TypingManager.cs
@@ -24,9 +24,44 @@ public class TypingManager : MonoBehaviour
     // 何番目の問題か
     private int _qNum;
 
+    // 問題の何文字目か
+    private int _aNum;
+
     // ゲームを始めた時に1度だけ呼ばれるもの
     void Start()
     {
+        // 問題を出す
+        OutPut();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(_aString[_aNum].ToString()))
+        {
+            // 正解
+            Correct();
+
+            // 最後の文字に正解したら
+            if (_aNum >= _aString.Length)
+            {
+                // 問題を変える
+                OutPut();
+            }
+        }
+        else if (Input.anyKeyDown)
+        {
+            // 失敗
+            Miss();
+        }
+    }
+
+    // 問題を出すための関数
+    void OutPut()
+    {
+        // 0番目の文字に戻す
+        _aNum = 0;
+
         // _qNumに０〜問題数の数までのランダムな数字を1つ入れる
         _qNum = Random.Range(0, _question.Length);
 
@@ -40,9 +75,18 @@ public class TypingManager : MonoBehaviour
         aText.text = _aString;
     }
 
-    // Update is called once per frame
-    void Update()
+    // 正解用の関数
+    void Correct()
     {
-        
+        // 正解した時の処理（やりたいこと）
+        _aNum++;
+        Debug.Log("正解したよ！");
+    }
+
+    // 間違え用の関数
+    void Miss()
+    {
+        // 間違えた時にやりたいこと
+        Debug.Log("間違えたよ！");
     }
 }
